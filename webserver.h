8ae7f2be403e76b913655f0aec6e65cdb58f5b66
Mr.Zhang <2631657310@qq.com>
2025-10-31 16:47:29 +0800

    部分注释webserver.h和webserver.cpp文件

--- webserver.h
+++ webserver.h
@@ -1,82 +1,85 @@
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <arpa/inet.h>	//提供IP地址转换的函数。
 #include <errno.h>
-#include <fcntl.h>
+#include <fcntl.h>		 //文件控制函数。
+#include <netinet/in.h>	 //提供Internet地址族（AF_INET / AF_INET6）相关定义
+#include <stdio.h>
 #include <stdlib.h>
-#include <cassert>
 #include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>	 //POSIX系统调用接口
 
-#include "./threadpool/threadpool.h"
+#include <cassert>	//断言
+
 #include "./http/http_conn.h"
+#include "./threadpool/threadpool.h"
 
-const int MAX_FD = 65536;           //最大文件描述符
-const int MAX_EVENT_NUMBER = 10000; //最大事件数
-const int TIMESLOT = 5;             //最小超时单位
+const int MAX_FD = 65536;			 // 最大文件描述符
+const int MAX_EVENT_NUMBER = 10000;	 // 最大事件数
+const int TIMESLOT = 5;				 // 最小超时单位
 
 class WebServer
 {
-public:
-    WebServer();
-    ~WebServer();
+   public:
+	WebServer();
+	~WebServer();
 
-    void init(int port , string user, string passWord, string databaseName,
-              int log_write , int opt_linger, int trigmode, int sql_num,
-              int thread_num, int close_log, int actor_model);
+	void init(int port, string user, string passWord, string databaseName, int log_write,
+			  int opt_linger, int trigmode, int sql_num, int thread_num, int close_log,
+			  int actor_model);
 
-    void thread_pool();
-    void sql_pool();
-    void log_write();
-    void trig_mode();
-    void eventListen();
-    void eventLoop();
-    void timer(int connfd, struct sockaddr_in client_address);
-    void adjust_timer(util_timer *timer);
-    void deal_timer(util_timer *timer, int sockfd);
-    bool dealclientdata();
-    bool dealwithsignal(bool& timeout, bool& stop_server);
-    void dealwithread(int sockfd);
-    void dealwithwrite(int sockfd);
+	void thread_pool();	 // 创建线程池
 
-public:
-    //基础
-    int m_port;
-    char *m_root;
-    int m_log_write;
-    int m_close_log;
-    int m_actormodel;
+	void sql_pool();  // 初始化数据库及连接
 
-    int m_pipefd[2];
-    int m_epollfd;
-    http_conn *users;
+	void log_write();
+	void trig_mode();
+	void eventListen();
+	void eventLoop();
+	void timer(int connfd, struct sockaddr_in client_address);
+	void adjust_timer(util_timer *timer);
+	void deal_timer(util_timer *timer, int sockfd);
+	bool dealclientdata();
+	bool dealwithsignal(bool &timeout, bool &stop_server);
+	void dealwithread(int sockfd);
+	void dealwithwrite(int sockfd);
 
-    //数据库相关
-    connection_pool *m_connPool;
-    string m_user;         //登陆数据库用户名
-    string m_passWord;     //登陆数据库密码
-    string m_databaseName; //使用数据库名
-    int m_sql_num;
+   public:
+	// 基础
+	int m_port;
+	char *m_root;
+	int m_log_write;
+	int m_close_log;
+	int m_actormodel;
 
-    //线程池相关
-    threadpool<http_conn> *m_pool;
-    int m_thread_num;
+	int m_pipefd[2];
+	int m_epollfd;
+	http_conn *users;
 
-    //epoll_event相关
-    epoll_event events[MAX_EVENT_NUMBER];
+	// 数据库相关
+	connection_pool *m_connPool;
+	string m_user;			// 登陆数据库用户名
+	string m_passWord;		// 登陆数据库密码
+	string m_databaseName;	// 使用数据库名
+	int m_sql_num;
 
-    int m_listenfd;
-    int m_OPT_LINGER;
-    int m_TRIGMode;
-    int m_LISTENTrigmode;
-    int m_CONNTrigmode;
+	// 线程池相关
+	threadpool<http_conn> *m_pool;
+	int m_thread_num;
 
-    //定时器相关
-    client_data *users_timer;
-    Utils utils;
+	// epoll_event相关
+	epoll_event events[MAX_EVENT_NUMBER];
+
+	int m_listenfd;
+	int m_OPT_LINGER;
+	int m_TRIGMode;
+	int m_LISTENTrigmode;
+	int m_CONNTrigmode;
+
+	// 定时器相关
+	client_data *users_timer;
+	Utils utils;
 };
 #endif
